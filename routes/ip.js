a8608140d574ddc64052e28f5e22a05a43c70367
Jason Rubenstein <rubenstein.jason.i@gmail.com>
2022-01-12 06:33:54 -0500

    test request-ip

--- routes/ip.js
+++ routes/ip.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const forwarded = require("forwarded");
+const requestIp = require("request-ip");
 
 router.get("/", async (req, res) => {
   res.status(200).send({
-    APPNAME: process.env.HEROKU_APP_NAME,
     forwardedFor: forwarded(req),
     ip: req.ip,
+    requestIP: requestIp.getClientIp(req),
   });
 });
 
